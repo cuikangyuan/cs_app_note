2d82b0d004e2d081de0dd03c66a651df54fb54fa
cuikangyuan3 <cuikangyuan@jd.com>
2022-01-12 17:55:15 +0800

    add elf note

--- linker/elf.c
+++ linker/elf.c
@@ -1,5 +1,14 @@
+#include<stdio.h>
+#include<elf.h>
+
 unsigned long long data1 = 0xd1d1d1d1d1d1d1d1;
 unsigned long long data2 = 0xd2d2d2d2d2d2d2d2;
 void func1() {};
 void func2() {};
 
+
+int main() {
+    Elf64_Sym sym;
+    printf("%ld \n", sizeof(sym));
+    return 0;
+}
