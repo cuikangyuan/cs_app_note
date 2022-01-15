4dbeaf534b74705afb65be61555da3b3ed465909
cuikangyuan3 <cuikangyuan@jd.com>
2022-01-15 22:20:40 +0800

    add note

--- test.c
+++ test.c
@@ -3,13 +3,38 @@
 
 int main() 
 {
-    int r = 2;
-    int c = 1;
+    // int r = 2;
+    // int c = 1;
 
-    for(int i=0; i<9; i++) {
-        int r1 = (r/3)*3 + i/3;
-        int c1 = (c/3)*3 + i%3;
-        printf("i=%d, (%d, %d)=(%d+%d, %d+%d)", i, r1, c1, ((r/3)*3), (i/3), ((c/3)*3), (i%3));
-        printf("\n");
-    }
+    // for(int i=0; i<9; i++) {
+    //     int r1 = (r/3)*3 + i/3;
+    //     int c1 = (c/3)*3 + i%3;
+    //     printf("i=%d, (%d, %d)=(%d+%d, %d+%d)", i, r1, c1, ((r/3)*3), (i/3), ((c/3)*3), (i%3));
+    //     printf("\n");
+    // }
+
+    int *p1[4] = {1, 2, 3, 4};
+	printf("%d\n", *p1);
+
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     printf("%c\n", *(p1 + i));
+    //     printf("%c\n", p1[i]);
+    // }
+    
+    // for (int i = 0; i < 4; i++) 
+    // {
+	//     printf("%d\n", *(p1 + i));
+	//     printf("%d\n", p1[i]);
+    // }
+
+    //int(*p2)[4] = &p1;
+
+    // for (int i = 0; i < 4; i++) 
+    // { 
+    //     printf("%d\n", *(*p2 + i));
+    //     printf("%d\n", **(p2 + i));//错误！！！增加指针类型大小的字节
+    // }
+
+    return 0;
 }
