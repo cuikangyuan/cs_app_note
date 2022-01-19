14726dd41c4cb9dd32ac74d4ab29765b83fbbbff
cuikangyuan <cuikangyuan@outlook.com>
2022-01-19 19:52:39 +0800

    add dynamic link note

---
+++ dynamicLink/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+//https://blog.csdn.net/linyt/article/details/51635768
+void print_banner()
+{
+    printf("Welcome to World of PLT and GOT\n");
+}
+
+int main(void) {
+    print_banner();
+    return 0;
+}
