a67b594a89a15d9612f76468abb5f42c2a3ba129
cuikangyuan3 <cuikangyuan@jd.com>
2022-01-23 13:29:32 +0800

    add test code

--- m.c
+++ m.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+#include <stdint.h>
+void swap() 
+{
 
-void swap();
+};
+
 int buf[2] = {1, 2};
 
 int main() {
     swap();
+    u_int64_t i = 1 << 2;
+    printf("%ld\n", i);
     return 0;
 }
