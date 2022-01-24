00180ee46d755711d870c4eeb45aa991a1ea9ef2
cuikangyuan <cuikangyuan@outlook.com>
2022-01-24 15:22:40 +0800

    modify project structure

---
+++ misc/m.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdint.h>
+void swap() 
+{
+
+};
+
+
+
+int buf[2] = {1, 2};
+
+int main() {
+    swap();
+    u_int64_t i = 1 << 2;
+    printf("%ld\n", i);
+    return 0;
+}
